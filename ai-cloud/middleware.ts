b90b731f53d7dcd92a303b49d63b268d398efbc2
devHksingh <hksingh.dev@gmail.com>
2024-09-18 07:51:24 +0530

    update clerkMiddleware

--- ai-cloud/middleware.ts
+++ ai-cloud/middleware.ts
@@ -13,26 +13,26 @@ const isPublicApiRoute = createRouteMatcher([
 
 ])
 
-export default clerkMiddleware((auth,req) =>{
+export default clerkMiddleware((auth, req) => {
     const {userId} = auth();
-    const currentUrl = new URL(req.url);
-    const isAccessingDashboard = currentUrl.pathname === "/home"
-    const isApiRequest = currentUrl.pathname.startsWith("/api")
+    const currentUrl = new URL(req.url)
+     const isAccessingDashboard = currentUrl.pathname === "/home"
+     const isApiRequest = currentUrl.pathname.startsWith("/api")
 
-
-    if(userId && isPublicRoute(req) && !isAccessingDashboard){
-        return NextResponse.redirect(new URL("/home",req.url))
+     // If user is logged in and accessing a public route but not the dashboard
+    if(userId && isPublicRoute(req) && !isAccessingDashboard) {
+        return NextResponse.redirect(new URL("/home", req.url))
     }
-
-    // not logged in
+    //not logged in
     if(!userId){
-        //  isf user is not logged in and trying to access a protected route
-        if(!isPublicRoute(req) && !isPublicApiRoute(req)){
-            return NextResponse.redirect(new URL("/sign-in",req.url))
+        // If user is not logged in and trying to access a protected route
+        if(!isPublicRoute(req) && !isPublicApiRoute(req) ){
+            return NextResponse.redirect(new URL("/sign-in", req.url))
         }
-        // if req is for a protected API and the user is not logged in
+
+        // If the request is for a protected API and the user is not logged in
         if(isApiRequest && !isPublicApiRoute(req)){
-            return NextResponse.redirect(new URL("/sign-in",req.url))
+            return NextResponse.redirect(new URL("/sign-in", req.url))
         }
     }
     return NextResponse.next()
